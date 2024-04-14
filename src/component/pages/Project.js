fd616fe3a41d3b3758e358ac0b13d3396f1030ee
bit-sercererx <ameerahmed202397@gmail.com>
2024-04-14 13:07:30 +0300

    done

--- src/component/pages/Project.js
+++ src/component/pages/Project.js
@@ -1,11 +1,11 @@
 import website from "../../assets/imgs/website.jpg";
 import { motion } from "framer-motion";
-import car from "../../assets/imgs/car.png";
-import fastfood from "../../assets/imgs/fastfood.png";
-import pokemon from "../../assets/imgs/pokemon.png";
-import space from "../../assets/imgs/space.png";
 import { PiGithubLogoFill } from "react-icons/pi";
-
+import car from "../../assets/imgs/car.png";
+import space from "../../assets/imgs/space.png";
+import pokemon from "../../assets/imgs/pokemon.png";
+import fastfood from "../../assets/imgs/fastfood.png";
+import movie from "../../assets/imgs/movie.png";
 function Project() {
   return (
     <div>
@@ -27,57 +27,91 @@ function Project() {
           id="img"
           className=" bg-orange hover:bg-lightOrange w-[300px] xl:w-[400px]"
         >
-          <img src={car} className="" />
+          <a href="https://shopping-web-site-86.netlify.app/" target="-blank">
+            <img src={car} className="" />
+          </a>
           <h1 className="font-bold pt-2">86</h1>
           <span className=" text-xs ">
-            This site was created by html and css
+            An online store for selling car modification parts has been
+            created:html/css{" "}
           </span>
-          <PiGithubLogoFill className="text-3xl pt-2" />
+          <a
+            href="https://github.com/bit-sercererx/shopping-web-site-86-"
+            target="-blank"
+          >
+            {" "}
+            <br />
+            <PiGithubLogoFill className="text-3xl pt-2 inline" />
+          </a>
         </div>
 
         <div
           id="img"
           className=" bg-orange hover:bg-lightOrange w-[300px] xl:w-[400px]"
         >
-          <img src={fastfood} />
-          <h1 className="font-bold pt-2">name project</h1>
+          <a href="https://fast-food-shop-01.netlify.app/" target="-blank">
+            <img src={fastfood} />
+          </a>
+          <h1 className="font-bold pt-2">Fast Food</h1>
           <span className=" text-xs ">
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui b
+            An online restaurant that sells fast food, created by : html/css{" "}
           </span>
-          <PiGithubLogoFill className="text-3xl pt-2" />
+          <br />
+          <a
+            href="https://github.com/bit-sercererx/fast-food-shop"
+            target="-blank"
+          >
+            <PiGithubLogoFill className="text-3xl pt-2 inline" />
+          </a>
         </div>
 
         <div
           id="img"
           className=" bg-orange hover:bg-lightOrange w-[300px] xl:w-[400px]"
         >
-          <img src={pokemon} />
-          <h1 className="font-bold pt-2">name project</h1>
+          <a href="https://pokemon-api-detils.netlify.app/" target="-blank">
+            <img src={pokemon} />
+          </a>
+          <h1 className="font-bold pt-2">pokemon</h1>
           <span className=" text-xs ">
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui b
+            A website displaying Pokemon and its details. It was created
+            by:html/css/js{" "}
           </span>
-          <PiGithubLogoFill className="text-3xl pt-2" />
+          <br />
+          <a
+            href="https://github.com/bit-sercererx/JS-Pokemon_API_Project"
+            target="-blank"
+          >
+            <PiGithubLogoFill className="text-3xl pt-2 inline" />
+          </a>
         </div>
 
         <div
           id="img"
           className=" bg-orange hover:bg-lightOrange w-[300px] xl:w-[400px]"
         >
-          <img src={space} />
-          <h1 className="font-bold pt-2">name project</h1>
+          <a href="https://our-space-01.netlify.app/" target="-blank">
+            <img src={space} />
+          </a>
+          <h1 className="font-bold pt-2">our space</h1>
           <span className=" text-xs ">
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui b
+            A website to display details of our space ,made by: html/css{" "}
           </span>
-          <PiGithubLogoFill className="text-3xl pt-2" />
+          <br />
+          <a href="https://github.com/bit-sercererx/our-space" target="-blank">
+            <PiGithubLogoFill className="text-3xl pt-2" />
+          </a>
         </div>
+
         <div
           id="img"
           className=" bg-orange hover:bg-lightOrange w-[300px] xl:w-[400px]"
         >
-          <img src={space} />
-          <h1 className="font-bold pt-2">name project</h1>
+          <img src={movie} />
+          <h1 className="font-bold pt-2">move</h1>
           <span className=" text-xs ">
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui b
+            A website for displaying and watching films and displaying actors
+            and their films. It was created by: html/js/react/tallwind{" "}
           </span>
           <PiGithubLogoFill className="text-3xl pt-2" />
         </div>
